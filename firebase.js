5259839fb40641aa7fe0cf290bd486ecd17668cb
gyu1226 <crg2468@naver.com>
2024-09-19 19:17:56 +0900

    백업
    백업

---
+++ firebase.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+
+const firebaseConfig = {
+  //firebaseConfig파일은 GitHub에 배포하면 안됨. (Key가 다 있기 때문)
+  apiKey: import.meta.env.VITE_APP_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DB_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MEG_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
+const storage = getStorage(app);
+const auth = getAuth(app);
